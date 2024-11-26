db624f17a2ddcecfffdd10186ddeb8efc345f1c0
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2024-11-26 11:38:50 -0500

    fgh

--- wrapper/test.cpp
+++ wrapper/test.cpp
@@ -7,24 +7,19 @@
 
 #include <vector>
 
-// Test the C++ workflow loaded from the legate/cupynumeric libraries
-
-// General Idea:
-    // 1. Construct Runtime
-    // 2. Use Runtime to create Logical Store
-    // 3. Use Logical Store to create NDArray
-    // 4. Use NDArray to call operations (e.g., dot)
+// This is the workflow I would like to 
+// re-create in Julia
 
 void test_workflow(){
     Runtime r();
-    std::vector<uint64_t> dims1 = {4,2};
+    std::vector<uint64_t> dims1 = {4,2}; 
     std::vector<uint64_t> dims2 = {2,4};
 
     Shape s1(dims1);
     Shape s2(dims2);
 
     PrimitiveType pt(3); // INT32
-    LogicalStore ls1 = r.create_store(s1, pt);
+    LogicalStore ls1 = r.create_store(s1, pt); //how to put data in here?
     LogicalStore ls2 = r.create_store(s2, pt);
 
     NDArray arr1(ls1);
@@ -36,4 +31,9 @@ void test_workflow(){
     dot(arr1, arr2); //& this returns nothing???
 
 
+}
+
+int main(int argc, char** argv){
+    test_workflow();
+    return 0;
 }
