9b76a86a4c54d0dc1fea3bf0d4f94d4cffaf388c
ejmeitz <ejmeitz1@gmail.com>
2024-12-10 21:25:50 -0500

    up test.cpp

--- wrapper/test.cpp
+++ wrapper/test.cpp
@@ -7,11 +7,11 @@
 // re-create in Julia
 
 cupynumeric::NDArray test_workflow(){
-    N = 25
-    auto arr1 = cupynumeric::full({N}, 2.0);
-    auto arr2 = cupynumeric::full({N}, 2.0);
+    int N = 25;
+    auto arr1 = cupynumeric::full({N}, legate::Scalar{2.0});
+    auto arr2 = cupynumeric::full({N}, legate::Scalar{2.0});
 
-    return cupynumeric::dot(arr1, arr2)
+    return cupynumeric::dot(arr1, arr2);
 }
 
 int main(int argc, char** argv){
@@ -22,7 +22,7 @@ int main(int argc, char** argv){
 
     auto res = test_workflow();
 
-    std::cout << res[0] << std::endl;
+    std::cout << res << std::endl;
 
     return legate::finish();
 }
