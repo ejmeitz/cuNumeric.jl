3ae02c11d5b00152dd2a551b4ae013514fa353f3
krasow <krasow@u.northwestern.edu>
2024-12-24 17:33:35 -0600

    modify env

--- build.sh
+++ build.sh
@@ -19,4 +19,4 @@ echo "Using Legate at $legate_root"
 cupynumeric_root=`python -c 'import cupynumeric.install_info as i; from pathlib import Path; print(Path(i.libpath).parent.resolve())'`
 echo "Using cuPyNumeric at $cupynumeric_root"
 cmake -S . -B build -GNinja -D legate_ROOT="$legate_root" -D cupynumeric_ROOT="$cupynumeric_root" -D CMAKE_BUILD_TYPE=Debug
-cmake --build build --parallel 8
+cmake --build build --parallel 8 --verbose
