74b491d3deb0ca288e526f6c331f4c04e479d91a
ejmeitz <ejmeitz1@gmail.com>
2024-12-27 15:46:41 -0500

    un break install script

--- scripts/install_cxxwrap.sh
+++ scripts/install_cxxwrap.sh
@@ -24,6 +24,8 @@ JULIA_CXXWRAP_SRC=$CUNUMERIC_JL_HOME/libcxxwrap-julia
 cd $JULIA_CXXWRAP_SRC
 
 # grab latest release of libcxxwrap-julia
+# in theory the latest libcxxwrap could be incompatible with latest CxxWrap
+  # if CxxWrap has not tagged a new version
 tag=$(git describe --tags `git rev-list --tags --max-count=1`)
 git checkout $tag
 
@@ -31,11 +33,11 @@ git checkout $tag
 JULIA_DEP_PATH=$($JULIA -e 'println(DEPOT_PATH[1])')
 
 # https://github.com/JuliaInterop/libcxxwrap-julia/tree/v0.13.3?tab=readme-ov-file#configuring-and-building
-JULIA_CXXWRAP=$JULIA_DEP_PATH/dev/libcxxwrap_julia_jll/
+JULIA_CXXWRAP=$JULIA_DEP_PATH/dev/libcxxwrap_julia_jll/override
 
 # Clean up whatever env is there right now and
 # build default version of CxxWrap / libcxxwrap_julia
-#* THIS COULD BREAK SOME USERS CODE IF THEY ALREADY DEV'D THIS PKG
+#* THIS COULD BREAK SOME USERS CODE IF THEY ALREADY OVERRIDE THIS PKG
 cd $CUNUMERIC_JL_HOME/pkg
 rm -rf "Manifest.toml"
 rm -rf $JULIA_CXXWRAP
@@ -45,6 +47,8 @@ julia -e 'using Pkg; Pkg.activate("."); Pkg.resolve(); Pkg.precompile(["CxxWrap"
 # this command will download https://github.com/JuliaBinaryWrappers/libcxxwrap_julia_jll.jl and install it in JULIA_DEP_PATH
 julia -e 'using Pkg; Pkg.activate("."); Pkg.develop(PackageSpec(name="libcxxwrap_julia_jll")); import libcxxwrap_julia_jll; libcxxwrap_julia_jll.dev_jll()'
 
+
+# JULIA_CXXWRAP_OVERRIDE=$JULIA_CXXWRAP/override/
 # Delete the default JLL installation of cxxwrap_julia
 rm -rf $JULIA_CXXWRAP 
 mkdir $JULIA_CXXWRAP
