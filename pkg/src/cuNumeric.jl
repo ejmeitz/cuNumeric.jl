f1ab09d4cd6c584e872ecd7b6d44d1522ad36cfc
ejmeitz <ejmeitz1@gmail.com>
2025-01-04 12:46:51 -0500

    start cli parser

--- pkg/src/cuNumeric.jl
+++ pkg/src/cuNumeric.jl
@@ -23,36 +23,28 @@ module cuNumeric
 # the above yields:: std::optional<cupynumeric::NDArray>
 
 using CxxWrap
+using Preferences
 
 lib = "libcupynumericwrapper.so"
 @wrapmodule(() -> joinpath(@__DIR__, "../", "../", "build", lib))
 
+include("settings.jl")
 include("ndarray.jl")
 
-# From https://github.com/JuliaGraphics/QML.jl/blob/dca239404135d85fe5d4afe34ed3dc5f61736c63/src/QML.jl#L147
-mutable struct ArgcArgv
-    argv
-    argc::Cint
-  
-    function ArgcArgv(args::Vector{String})
-      argv = Base.cconvert(CxxPtr{CxxPtr{CxxChar}}, args)
-      argc = length(args)
-      return new(argv, argc)
-    end
-  end
-  
-getargv(a::ArgcArgv) = Base.unsafe_convert(CxxPtr{CxxPtr{CxxChar}}, a.argv)
-global ARGV::ArgcArgv  
+
+global ARGV::ArgcArgv
 
 # Runtime initilization
 # Called once in lifetime of code
 function __init__()
     @initcxx
 
-    # BREAK THE LEGATE/CUNUMERIC OPTIONS OUT INTO CLASS
-    # SO THEY CAN BE STARTED AND STOPPED MULTIPLE TIMES
-    # INSIDE OF A SINGLE JULIA CODE WITH DIFFERENT ARGS
-    global ARGV = ArgcArgv([Base.julia_cmd()[1], ARGS...])
+    if @has_preference("use_local_prefs")
+      global ARGV = ArgcArgv([Base.julia_cmd()[1], get_initial_legate_settings()...])
+    else
+      global ARGV = ArgcArgv([Base.julia_cmd()[1], ARGS...])
+    end
+    
     
     res = cuNumeric.start_legate(ARGV.argc, getargv(ARGV))
     if res == 0
---
+++ pkg/src/settings.jl
@@ -0,0 +1,86 @@
+export LegateSettings
+
+# From https://github.com/JuliaGraphics/QML.jl/blob/dca239404135d85fe5d4afe34ed3dc5f61736c63/src/QML.jl#L147
+mutable struct ArgcArgv
+    argv
+    argc::Cint
+  
+    function ArgcArgv(args::Vector{String})
+      argv = Base.cconvert(CxxPtr{CxxPtr{CxxChar}}, args)
+      argc = length(args)
+      return new(argv, argc)
+    end
+  end
+  
+getargv(a::ArgcArgv) = Base.unsafe_convert(CxxPtr{CxxPtr{CxxChar}}, a.argv)
+
+#######################################
+
+@enum Launchers mpirun jsrun srun none not_passed
+
+# Just assuming the args here are the same that cupynumeric
+#  expects when initializing legate
+# https://docs.nvidia.com/legate/latest/usage.html#resource-allocation
+Base.@kwdef mutable struct LegateSettings
+  cpus::Int = -1 # how many individual CPU threads are spawned
+  omps::Int = -1 # how many OpenMP groups are spawned
+  ompthreads::Int = -1 # how many threads are spawned per OpenMP group
+  gpus::Int = -1 # how many GPUs are used
+
+  nodes::Int = -1 # number of nodes to use
+  launcher::Launchers = not_passed # program to spawn processes (e.g. mpirun)
+
+  # Also memory options, but I'm just going to let Legate
+  # do that until someone asks for that feature
+end
+
+# Returns as list of strings (how argv expects minus prog name)
+function make_cmd(ls::LegateSettings)
+  cmd = ""
+
+  if nodes > 0 && launcher != not_passed
+    cmd *= "--nodes $(ls.nodes) --launcher $(String(Symbol(ls.launcher)))"
+  elseif nodes > 0 && launcher == not_passed
+    throw("Asked for $(nodes) nodes but did not pass a launcher.")
+  end
+
+  if cpus > 0
+    cmd *= "--cpus $(ls.cpus)"
+  end
+
+  if gpus > 0
+    cmd *= "--gpus $(ls.gpus)"
+  end
+
+  if omps > 0
+    cmd *= "--omps $(ls.omps)"
+  end
+
+  if ompthreads > 0
+    cmd *= "--ompthreads $(ls.ompthreads)"
+  end
+
+  return cmd.split()
+end
+
+# Parse from LocalPreferences.toml if it exists
+# These are the settings used inside __init__ 
+# to start legate the first time
+function get_initial_legate_settings()
+  defaults = LegateSettings()
+
+  for opt in keys(legate_default_options)
+    if @has_preference(opt)
+
+    end
+  end
+  return make_cmd(defaults)
+end
+
+#TODO
+Base.show(io::IO, ls::LegateSettings) = print("THESE ARE THE LEGATE SETTINGS, NOT IMPLEMENTED")
+
+function legate_capabilities()
+  #output from legate --info
+  # HOW???
+end
