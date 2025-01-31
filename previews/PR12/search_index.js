b90be869af6b1012085ba773a7790a7ce8c5cd28
Documenter.jl <documenter@juliadocs.github.io>
2025-01-31 16:19:00 +0000

    build based on 310c6f5

---
+++ previews/PR12/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"benchmark/","page":"Benchmarking","title":"Benchmarking","text":"Ethan do this so Wonchan can benchmark!","category":"page"},{"location":"","page":"Home","title":"Home","text":"You've found the home page!","category":"page"}]
+}
