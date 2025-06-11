235c51fb0376294f4769cfb26179e90a6f4edc1b
krasow <krasow@u.northwestern.edu>
2025-06-11 13:46:56 -0500

    btime adjustments

--- overheads/cunumeric-c-api/c-ndarray.jl
+++ overheads/cunumeric-c-api/c-ndarray.jl
@@ -1,6 +1,5 @@
 using cuNumeric
-using InteractiveUtils
-
+using BenchmarkTools
 
 @noinline function loop_array_no_time(iters::Integer, size::Integer = 500_000)
     for _ in range(1,iters)
@@ -9,4 +8,4 @@ using InteractiveUtils
     end
 end
 
-@time loop_array_no_time(1000)
+@btime loop_array_no_time(1000)
